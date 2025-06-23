190a806bd7651a66a2a103d880b8e50fa46f3458
yaopeng.kang <78490223@qq.com>
2025-06-23 04:36:37 +0000

    add ahb_agent to compile

---
+++ dv/simulation/verif_env/soc/soc_top/filelist/ahb.f
@@ -0,0 +1,15 @@
+//////////////////////////////
+//directory
++incdir+$PROJ_HOME/dv/simulation/verif_env/soc/common/uvc/ahb_pkg
++incdir+$PROJ_HOME/dv/simulation/verif_env/soc/common/uvc/ahb_pkg/vutils_pkg
+
+//////////////////////////////
+//package
+$PROJ_HOME/dv/simulation/verif_env/soc/common/uvc/ahb_pkg/vutils_pkg/vutils_pkg.sv
+$PROJ_HOME/dv/simulation/verif_env/soc/common/uvc/ahb_pkg/ahb_if.sv
+$PROJ_HOME/dv/simulation/verif_env/soc/common/uvc/ahb_pkg/ahb_pkg.sv
+
+//////////////////////////////
+//interface
+
+
